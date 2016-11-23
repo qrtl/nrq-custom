f707802b9e72f41f9d1f03ef34070a0b9d343b16
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-11-23 10:53:56 +0800

    [ADD] sale_price_unchange

---
+++ sale_price_unchange/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Rooms For (Hong Kong) Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Price Unchange",
+    "summary": "SO line qty change should not trigger price update",
+    "version": "10.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.odoo-asia.com/",
+    "author": "Rooms For (Hong Kong) Limited",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "pre_init_hook": "",
+    "post_init_hook": "",
+    "post_load": "",
+    "uninstall_hook": "",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "sale",
+    ],
+    "data": [
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
