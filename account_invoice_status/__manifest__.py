4554c74dc46c8c31ab19d9d701a8a1cfe0f1cfa8
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-11-29 23:18:25 +0800

    [ADD] account_invoice_status

---
+++ account_invoice_status/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Rooms For (Hong Kong) Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account Invoice Status",
+    "summary": "Make the invoice validation operation easier",
+    "version": "10.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://www.odoo-asia.com/",
+    "author": "Rooms For (Hong Kong) Limited",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "pre_init_hook": "",
+    "post_init_hook": "",
+    "post_load": "",
+    "uninstall_hook": "",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "account",
+    ],
+    "data": [
+        'views/account_invoice_view.xml'
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
