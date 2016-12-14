3c05aeb09f1222fcafc24ed6dc6d7803b4ddad36
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-12-14 10:01:33 +0800

    [FIX] update names ids to follow standard

---
+++ sale_report_nrq/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Rooms For (Hong Kong) Limited T/A OSCG
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Sales Document Print",
+    "summary": "",
+    "version": "10.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.odoo-asia.com/",
+    "author": "Rooms For (Hong Kong) Limited T/A OSCG",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "pre_init_hook": "",
+    "post_init_hook": "",
+    "post_load": "",
+    "uninstall_hook": "",
+    "external_dependencies": {
+        "python": [],
+        "bin": [],
+    },
+    "depends": [
+        "nrq_base",
+    ],
+    "data": [
+        'views/sale_quotation_template.xml'
+    ],
+    "demo": [
+    ],
+    "qweb": [
+    ]
+}
