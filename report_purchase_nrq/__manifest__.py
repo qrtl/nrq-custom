0ac25bd329670dc5c4296ad2e5a1e893b9488a18
Yoshi Tashiro <tashiro@roomsfor.hk>
2017-02-03 13:52:10 +0800

    Purchase document print function (#11)
    * [IMP] add fields to purchase.order, update common_template
    
    * [IMP] misc. adjustments on purchase print layout
    
    * [ADD] report_purchase_nrq
    
    * [IMP] update translation for report_common_nrq
    
    * [IMP] update translation

--- report_purchase_nrq/__manifest__.py
+++ report_purchase_nrq/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2017 Rooms For (Hong Kong) Limited T/A OSCG
+# Copyright 2017 Rooms For (Hong Kong) Limited T/A OSCG
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Report Common",
+    "name": "Purchase Document Print",
     "summary": "",
-    "version": "10.0.1.1.0",
-    "category": "Uncategorized",
+    "version": "10.0.1.0.0",
+    "category": "Purchases",
     "website": "https://www.odoo-asia.com/",
     "author": "Rooms For (Hong Kong) Limited T/A OSCG",
     "license": "AGPL-3",
@@ -20,19 +20,11 @@
         "bin": [],
     },
     "depends": [
-        "account",
-        "sale",
         "purchase",
-        "base_partner_number",
+        "report_common_nrq",
     ],
     "data": [
-        'data/res_partner_data.xml',
-        'views/res_company_views.xml',
-        'views/res_partner_views.xml',
-        'views/sale_order_views.xml',
-        'views/purchase_order_views.xml',
-        'views/account_invoice_views.xml',
-        'report/common_template.xml',
+        'report/purchase_report_template.xml'
     ],
     "demo": [
     ],
