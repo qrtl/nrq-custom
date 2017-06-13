0d307a8ea75d389bb93c99ad54b24777c1a526fc
tslai <tl@quartile.co>
2017-06-13 15:55:52 +0800

    [FIX] Customize Quotation View

--- report_common_nrq/models/sale_order.py
+++ report_common_nrq/models/sale_order.py
@@ -16,9 +16,36 @@ class SaleOrder(models.Model):
         string="Doc Title",
     )
 
+    approval = fields.Boolean(
+        string='Approval',
+        compute='_update_approve',
+        store=True,
+        readonly=True,
+        default=False,
+        copy=False,
+    )
 
     @api.multi
     def _prepare_invoice(self):
         res = super(SaleOrder, self)._prepare_invoice()
         res.update({'doc_title': self.doc_title})
         return res
+
+    @api.multi
+    @api.depends('state', 'approval')
+    def _update_approve(self):
+        for inv in self:
+            if inv.state == 'sent' and inv.approval:
+                inv.approval == True
+            elif inv.state != 'draft':
+                inv.approval == False
+
+    @api.multi
+    def action_approve(self):
+        for inv in self:
+            inv.approval = True
+
+    @api.multi
+    def action_disapprove(self):
+        for inv in self:
+            inv.approval = False
