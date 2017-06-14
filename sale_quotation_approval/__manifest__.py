0fb05f2298e69395d15e648b9aacfb5a9c5432fc
tslai <tl@quartile.co>
2017-06-14 12:28:52 +0800

    [FIX] Typo in comments

--- sale_quotation_approval/__manifest__.py
+++ sale_quotation_approval/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Quotation Approval",
     "summary": "",
