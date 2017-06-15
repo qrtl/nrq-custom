3384b025bfb4c31684b21be7fa1aca940e99a3c0
TimLai125 <tl@quartile.co>
2017-06-15 16:47:26 +0800

    [FIX] Remove compute attribute (#33)
    * [FIX] Remove compute attribute
    
    * Create sale_order.py

--- sale_quotation_approval/models/sale_order.py
+++ sale_quotation_approval/models/sale_order.py
@@ -10,8 +10,6 @@ class SaleOrder(models.Model):
 
     approval = fields.Boolean(
         string='Approved',
-        compute='_update_approve',
-        store=True,
         readonly=True,
         default=False,
         copy=False,
