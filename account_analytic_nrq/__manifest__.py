b85c314b603af55b3fdc77bc163a553b432c6dbf
tslai <tl@quartile.co>
2018-01-12 07:15:33 +0000

    [ADD] account_analytic_nrq

---
+++ account_analytic_nrq/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Adjustments on Account Analytic Functions",
+    "summary": "",
+    "description": """
+    """,
+    "version": "10.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://www.odoo-asia.com/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "pre_init_hook": "",
+    "post_init_hook": "",
+    "post_load": "",
+    "uninstall_hook": "",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_analytic_line_views.xml",
+    ],
+}
---
+++ account_analytic_nrq/models/account_analytic_line.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class AccountAnalyticLine(models.Model):
+    _inherit = 'account.analytic.line'
+
+    move_partner_id = fields.Many2one(
+        related='move_id.partner_id',
+        string='Move Line Partner',
+        readonly=True,
+        store=True,
+    )
+
+
