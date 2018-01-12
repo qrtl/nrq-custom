91dd37c36346caa068ba000fbfbe97faf8432f4b
Tim <tl@quartile.co>
2018-01-12 15:46:32 +0800

    Account_analytic_nrq (#38)
    * [ADD] account_analytic_nrq
    
    * [FIX] Adjusting the __manifest__.py
    
    * [FIX] Adjust styles

---
+++ account_analytic_nrq/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Adjustments on Account Analytic Functions",
+    "summary": "",
+    "description": """
+* Add partner_id of the move_id to account.analytic.line
+* Adjust the list view of the account.analytic.line
+    """,
+    "version": "10.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://www.odoo-asia.com/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_analytic_line_views.xml",
+    ],
+}
