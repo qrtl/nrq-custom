428a473e859569c960e8bb0b33244973f86d3a61
Yoshi Tashiro <tashiro@quartile.co>
2018-06-01 11:20:48 +0000

    [ADD] translation_adj

---
+++ translation_adj/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Translation Adjustments",
+    "summary": "",
+    "version": "10.0.1.0.0",
+    "category": "Other",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+    ],
+}
