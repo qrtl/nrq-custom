3b79c08db713fa375aae31a8d3aeda640bb70ebb
Tim <tl@quartile.co>
2018-06-14 15:11:51 +0800

    [IMP] Change the default view of Project>Dashboard to list view (#68)

--- project_view_adjust_nrq/__manifest__.py
+++ project_view_adjust_nrq/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Rooms For (Hong Kong) Limited T/A OSCG
+# Copyright 2016-2018 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Project View Adjust",
     "summary": "",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.1.0",
     "category": "Project",
-    "website": "https://www.odoo-asia.com/",
-    "author": "Rooms For (Hong Kong) Limited T/A OSCG",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
