9593f0b4c07a745a109f4ca2aa78105b06b03418
Yoshi Tashiro <tashiro@roomsfor.hk>
2018-10-17 23:05:24 +0900

    Sales Order Project Adjustments
    Merge pull request #78 from yostashiro/add-sale_order_project
    
    Add sale order project
    
    Sale Order Project Adjustments (#92)

---
+++ sale_order_project_nrq/models/sale.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, api
+
+
+class SaleOrder(models.Model):
+    _inherit = "sale.order"
+
+    def _prepare_project_vals(self):
+        self.ensure_one()
+        name = u" %s - %s" % (
+            self.name,
+            self.doc_title
+        )
+        return {
+            'user_id': self.user_id.id,
+            'name': name,
+            'partner_id': self.partner_id.id,
+            'allow_timesheets': False,
+        }
+
+    @api.multi
+    def write(self, vals):
+        if 'state' in vals and vals['state'] in ('sale', 'cancel'):
+            for order in self:
+                if order.project_project_id:
+                    order.update_project_allow_timesheets(vals['state'])
+        return super(SaleOrder, self).write(vals)
+
+    def update_project_allow_timesheets(self, state):
+        if state == 'sale':
+            self.project_project_id.allow_timesheets = True
+        if state == 'cancel':
+            related_sales_orders = self.search([
+                ('project_id', '=',
+                 self.project_project_id.analytic_account_id.id),
+                ('state', 'in', ('sale', 'done')),
+                ('id', '!=', self.id),
+            ])
+            if not related_sales_orders:
+                self.project_project_id.allow_timesheets = False
