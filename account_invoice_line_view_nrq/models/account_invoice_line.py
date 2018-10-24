9051a891998644559a10d1ddcedebb5c768e0c42
tslai <tl@quartile.co>
2018-10-24 07:21:34 +0000

    [ADD] account_invoice_line_view_nrq

---
+++ account_invoice_line_view_nrq/models/account_invoice_line.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields, api
+
+
+class AccountInvoiceLine(models.Model):
+    _inherit = 'account.invoice.line'
+
+    commercial_partner_id = fields.Many2one(
+        'res.partner',
+        related='invoice_id.commercial_partner_id',
+        string='Partner Company',
+        store=True,
+        readonly=True,
+    )
+    state = fields.Selection(
+        related='invoice_id.state',
+        string='Invoice Status',
+        store=True
+    )
