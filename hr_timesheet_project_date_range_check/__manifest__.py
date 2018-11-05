86e96e83dd91078487c8160ebeaa3e92d98b22ba
Tim <tl@quartile.co>
2018-11-05 18:17:57 +0800

    Sale Order Project Adjustments (#92)

---
+++ hr_timesheet_project_date_range_check/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Checking project date in timesheet',
+    'summary': '',
+    'description': '''
+    ''',
+    'version': '10.0.1.1.0',
+    'category': 'HR',
+    'website': 'https://www.quartile.co/',
+    'author': 'Quartile Limited',
+    'license': 'AGPL-3',
+    'depends': [
+        'hr_timesheet_sheet',
+    ],
+    'data': [
+    ],
+    'application': False,
+    'installable': True,
+}
