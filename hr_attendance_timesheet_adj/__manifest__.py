1db2c40a8f1180adeacd77e7cfd22dfbaace74c1
tslai <tl@quartile.co>
2018-11-07 07:05:24 +0000

    [FIX] Adjust timesheet end date range

--- hr_attendance_timesheet_adj/__manifest__.py
+++ hr_attendance_timesheet_adj/__manifest__.py
@@ -6,7 +6,7 @@
     'summary': '',
     'description': '''
     ''',
-    'version': '10.0.1.1.0',
+    'version': '10.0.1.2.0',
     'category': 'HR',
     'website': 'https://www.quartile.co/',
     'author': 'Quartile Limited',
