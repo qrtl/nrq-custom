864944af94fed3bb9aeea8490edf9cdb2b727f8b
Yoshi Tashiro <tashiro@quartile.co>
2018-12-27 21:25:53 +0800

    show nationality, adjust stat button

--- l10n_jp_hr_employee/models/hr_employee.py
+++ l10n_jp_hr_employee/models/hr_employee.py
@@ -22,13 +22,25 @@ class HrEmployee(models.Model):
         'hr.private.info',
         'employee_id',
         string='Private Info')
-    # private_info_id = fields.Many2one(
-    #     'hr.private.info',
-    #     compute='_compute_private_info_id',
-    #     string='Latest Private Info',
-    # )
-    #
-    # def _compute_private_info_id(self):
-    #     PrivateInfo = self.env['hr.private.info']
-    #     for employee in self:
-    #         employee.private_info_id = PrivateInfo.search([('employee_id', '=', employee.id)], order='date_start desc', limit=1)
+    private_info_id = fields.Many2one(
+        'hr.private.info',
+        compute='_compute_private_info_id',
+        string='Latest Private Info',
+    )
+    private_info_count = fields.Integer(
+        compute='_compute_private_info_count',
+        string='Private Info'
+    )
+
+    def _compute_private_info_id(self):
+        for info in self:
+            info.private_info_id = info.private_info_ids[:1].id
+
+    def _compute_private_info_count(self):
+        private_info_data = self.env['hr.private.info'].sudo().read_group(
+            [('employee_id', 'in', self.ids)],
+            ['employee_id'], ['employee_id'])
+        result = dict((data['employee_id'][0], data['employee_id_count'])
+                      for data in private_info_data)
+        for employee in self:
+            employee.private_info_count = result.get(employee.id, 0)
