04f543be51e9b73e435ee2ddcca56422f0ccc340
Yoshi Tashiro <tashiro@quartile.co>
2019-01-14 22:36:58 +0800

    open form view instead of tree if there is only one record

--- l10n_jp_hr_employee/models/hr_employee.py
+++ l10n_jp_hr_employee/models/hr_employee.py
@@ -41,3 +41,20 @@ class HrEmployee(models.Model):
                       for data in private_info_data)
         for employee in self:
             employee.private_info_count = result.get(employee.id, 0)
+
+    @api.multi
+    def action_view_private_info(self):
+        private_info = self.env['hr.private.info'].search(
+            [('employee_id', '=', self.id)])
+        action = self.env.ref(
+            'l10n_jp_hr_employee.act_hr_employee_2_hr_private_info').read()[0]
+        if len(private_info) > 1:
+            action['domain'] = [('id', 'in', private_info.ids)]
+        elif len(private_info) in [0, 1]:
+            action['views'] = [(self.env.ref(
+                'l10n_jp_hr_employee.view_hr_private_info_form').id, 'form')]
+            if len(private_info) == 1:
+                action['res_id'] = private_info.ids[0]
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+        return action
