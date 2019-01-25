969b340c9749f0034f649a7d0319674f3c41df8f
Tim <tl@quartile.co>
2019-01-25 14:36:52 +0800

    [ADD] product_permission_adj_nrq (#108)

---
+++ product_permission_adj_nrq/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Permission Adjustments",
+    "summary": "",
+    "version": "10.0.1.0.0",
+    "category": "Product",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "hr_expense",
+        "hr_timesheet",
+        "purchase",
+        "sale",
+        "stock",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "security/ir_model_access.xml",
+    ],
+}
