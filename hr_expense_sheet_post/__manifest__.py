a0650ad7d18a72f40adcad2a8810865525920a11
tslai <tl@quartile.co>
2019-01-28 17:36:47 +0800

    [ADD] hr_expense_sheet_post

---
+++ hr_expense_sheet_post/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Post Expense Reports",
+    "summary": "",
+    "description": """
+* Allows user to post multiple expense reports at the same time.
+    """,
+    "version": "10.0.1.0.0",
+    "category": "Expense",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "hr_expense",
+    ],
+    "data": [
+        'data/ir_actions.xml',
+    ],
+}
