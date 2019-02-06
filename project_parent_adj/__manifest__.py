a4fe2cee9abb827eb3019208d3102c171ae5f679
Yoshi Tashiro <tashiro@quartile.co>
2019-02-06 22:17:32 +0800

    hide parent project in list view

--- project_parent_adj/__manifest__.py
+++ project_parent_adj/__manifest__.py
@@ -7,7 +7,7 @@
     'description': """
 - Hide Parent Project field (expected to be temporarily).
         """,
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'license': 'AGPL-3',
     'category': 'project',
     'author': 'Quartile Limited',
