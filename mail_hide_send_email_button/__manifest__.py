52542c88764cc2105bc054498705b2ec182c1b67
tslai <tl@quartile.co>
2019-02-08 14:03:49 +0800

    [ADD] mail_hide_send_email_button

---
+++ mail_hide_send_email_button/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Hiding 'Send by Email' button",
+    "summary": "",
+    "description": """
+* Hides "Send by Email" button in following form views
+    - Sales Order
+    - Purchase Order
+    - Invoice
+    """,
+    "version": "10.0.1.0.0",
+    "category": "Base",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "account",
+        "purchase",
+        "sale",
+    ],
+    "data": [
+        'views/account_invoice_views.xml',
+        'views/purchase_order_views.xml',
+        'views/sale_order_views.xml',
+    ],
+}
