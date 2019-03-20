56bfde8c75a06ea83efd584883908836d368c17d
Tim <tl@quartile.co>
2019-03-20 11:46:25 +0800

    [IMP] Add display tax option to purchase order (#131)

--- report_purchase_nrq/__manifest__.py
+++ report_purchase_nrq/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016-2017 Quartile Limited
+# Copyright 2016-2019 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Purchase Document Print",
     "summary": "",
-    "version": "10.0.1.1.1",
+    "version": "10.0.2.0.0",
     "category": "Purchases",
-    "website": "https://www.odoo-asia.com/",
+    "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
     "license": "LGPL-3",
     "application": False,
@@ -24,7 +24,8 @@
         "report_common_nrq",
     ],
     "data": [
-        'report/purchase_report_template.xml'
+        'report/purchase_report_template.xml',
+        'views/purchase_order_views.xml',
     ],
     "demo": [
     ],
