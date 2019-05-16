23563c24cc975472ab9bd702dd67a89b6ce61757
tslai <tl@quartile.co>
2019-05-16 15:22:15 +0800

    [FIX] Permission Error when create hr.dependant

--- l10n_jp_hr_employee/models/res_company.py
+++ l10n_jp_hr_employee/models/res_company.py
@@ -10,3 +10,15 @@ class ResCompany(models.Model):
 
     appointment_letter_url = fields.Char('Letter of Appointment Link('
                                          'Dependant)')
+
+    @api.multi
+    def write(self, vals):
+        #FIXME: Not the best way to handle, when hr.dependant is
+        #       created/updated, the write method is triggered and pass
+        #       'appointment_letter_url' to update the res.company record
+        #       that would lead to a permission error.
+        if 'appointment_letter_url' in vals and not \
+            self.env.user.has_group('base.group_system'):
+            vals.pop('appointment_letter_url')
+        if vals:
+            return super(ResCompany, self).write(vals)
