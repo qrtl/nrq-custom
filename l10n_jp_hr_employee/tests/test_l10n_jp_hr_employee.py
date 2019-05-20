8f146cb9f9fe7306d04e64b152214a10b1af264e
tslai <tl@quartile.co>
2019-05-20 17:32:05 +0800

    [IMP] Add tests and post_install

--- l10n_jp_hr_employee/tests/test_l10n_jp_hr_employee.py
+++ l10n_jp_hr_employee/tests/test_l10n_jp_hr_employee.py
@@ -7,6 +7,7 @@ from odoo.exceptions import ValidationError
 
 
 class L10nJpHrEmployee(common.TransactionCase):
+    post_install = True
 
     def setUp(self):
         super(L10nJpHrEmployee, self).setUp()
@@ -14,56 +15,24 @@ class L10nJpHrEmployee(common.TransactionCase):
         self.test_user = self.env.ref('base.user_demo')
         # Employee record that linked to Demo user
         self.test_employee = self.env.ref('hr.employee_qdp')
-        # Japan
-        self.japan = self.env.ref('base.jp')
 
     def test_00_create_private_information(self):
-        with self.assertRaises(ValidationError):
-            self.env['hr.private.info'].sudo(self.test_user.id).create({
-                'employee_id': self.test_employee.id,
-                'temp_save': False,
-                'private_country_id': self.japan.id,
-                'roman_family_name': '山田',
-                'roman_given_name': '太郎',
-                'birthday': '2005/03/01',
-                'gender': 'male',
-                'private_phone': '09011112222',
-                'private_email': 'test@test.com',
-                'postal_code': '1020082',
-                'address_pref': 'Tokyo',
-                'address_street': '千代田区一番町１０－２２',
-                'building': '千代田一番町郵便局',
-                'residence_cert': False,
-                'residence_cert_filename': False,
-                'furi_address': 'チヨダク イチバンチョウ１０－２２',
-                'furi_building': 'チヨダイチバンチョウユウビンキョク',
-                'emerg_contact_type': 'spouse',
-                'emerg_contact_desc': False,
-                'emerg_contact_name': '山田　○○',
-                'emerg_contact_postal_code': '1020082',
-                'emerg_contact_address': '千代田区一番町１０－２２',
-                'emerg_contact_phone': '09011112222',
-                'bank_name': '三井住友銀行',
-                'bank_branch': '神保町支店',
-                'bank_acc_type': 'savings',
-                'bank_acc_number': '1234567',
-                'furi_bank_acc_holder': 'ﾔﾏﾀﾞﾀﾛｳ',
-                'school_name': '○○大学',
-                'school_dept_name': '○○学部○○○学科',
-                'school_completion': 'completed',
-                'school_completion_desc': False,
-                'year_left_school': '2000',
-                'qualification_ids': False,
-                'pension_code': '9999',
-                'pension_seq': '999999',
-                'emp_ins_number_1st': '9999',
-                'emp_ins_number_2nd': '999999',
-                'emp_ins_number_3rd': '9',
-                'note': 'Test',
-                'visa_number': False,
-                'date_visa_expiry': False,
-                'work_permit_number': False,
-                'residence_card': False,
-                'residence_card_filename': False,
-                'state': 'draft',
-            })
+        self.env['hr.private.info'].sudo(self.test_user.id).create({
+            'employee_id': self.test_employee.id,
+        })
+
+    def test_01_create_dependant_information(self):
+        private_info = self.env['hr.private.info'].sudo(self.test_user.id).create({
+            'employee_id': self.test_employee.id,
+        })
+        self.env['hr.dependant'].sudo(self.test_user.id).create({
+            'private_info_id': private_info.id,
+            'employee_id': self.test_employee.id,
+            'dependant_categ': '01_spouse',
+            'gender': 'male',
+            'residence_categ': 'together',
+            'income': 0,
+            'birthday': '2000/01/01',
+            'furi_name': 'ﾔﾏﾀﾞ ﾀﾛｳ',
+            'name': '山田 太郎',
+        })
