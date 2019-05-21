0209675ecb40e8ca08d1225f207ab33032eff0e2
Tim <tl@quartile.co>
2019-05-21 13:54:08 +0800

    [FIX] Add readonly to appointment_letter_url (#149)

---
+++ l10n_jp_hr_employee/tests/test_l10n_jp_hr_employee.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests import common
+from odoo.exceptions import ValidationError
+
+
+class L10nJpHrEmployee(common.TransactionCase):
+    post_install = True
+
+    def setUp(self):
+        super(L10nJpHrEmployee, self).setUp()
+        # Demo user
+        self.test_user = self.env.ref('base.user_demo')
+        # Employee record that linked to Demo user
+        self.test_employee = self.env.ref('hr.employee_qdp')
+
+    def test_00_create_private_information(self):
+        self.env['hr.private.info'].sudo(self.test_user.id).create({
+            'employee_id': self.test_employee.id,
+        })
+
+    def test_01_create_dependant_information(self):
+        # AccessError was rasied before adding "readonly=True" to
+        # appointment_letter_url during creation. Here we test the creation
+        # of hr.dependant with demo user
+        private_info = self.env['hr.private.info'].sudo(self.test_user.id).create({
+            'employee_id': self.test_employee.id,
+        })
+        self.env['hr.dependant'].sudo(self.test_user.id).create({
+            'private_info_id': private_info.id,
+            'employee_id': self.test_employee.id,
+            'dependant_categ': '01_spouse',
+            'gender': 'male',
+            'residence_categ': 'together',
+            'income': 0,
+            'birthday': '2000/01/01',
+            'furi_name': 'ﾔﾏﾀﾞ ﾀﾛｳ',
+            'name': '山田 太郎',
+        })
