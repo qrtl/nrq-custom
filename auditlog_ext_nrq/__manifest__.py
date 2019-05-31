0e95340f2e7a30af46c9de234a8f48461b5d7896
tslai <tl@quartile.co>
2019-05-31 10:49:17 +0800

    [ADD] auditlog_ext_nrq

---
+++ auditlog_ext_nrq/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "Audit Log Extension NRQ",
+    'version': "10.0.1.0.0",
+    'license': 'AGPL-3',
+    'author': 'Quartile Limited',
+    'website': 'http://www.quartile.co/',
+    'category': "Tools",
+    'depends': [
+        'auditlog',
+    ],
+    'data': [
+        'views/auditlog_log_views.xml',
+    ],
+    'application': True,
+    'installable': True,
+}
---
+++ auditlog_ext_nrq/tests/test_auditlog_ext_nrq.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests import common
+
+
+class TestAuditlogExtNrq(common.TransactionCase):
+    post_install = True
+
+    def setUp(self):
+        super(TestAuditlogExtNrq, self).setUp()
+        self.translation_model_id = self.env.ref('base.model_ir_translation').id
+        self.translation_rule = self.env['auditlog.rule'].create({
+            'name': 'Test rule for translation',
+            'model_id': self.translation_model_id,
+            'log_read': True,
+            'log_create': True,
+            'log_write': True,
+            'log_unlink': True,
+            'state': 'subscribed',
+            'log_type': 'full',
+        })
+        self.translation_record = self.env['ir.translation'].create(dict(
+            name="Test Translation Record"
+        ))
+
+    def test_00_create_record(self):
+        log = self.env['auditlog.log'].search([
+            ('model_id', '=', self.translation_model_id),
+            ('method', '=', 'create'),
+            ('res_id', '=', self.translation_record.id),
+        ], limit=1, order='id desc')
+        self.assertEqual(log.log_category, 'other')
+
+    def test_01_update_record_with_state(self):
+        self.translation_record.update({
+            'state': 'translated'
+        })
+        log = self.env['auditlog.log'].search([
+            ('model_id', '=', self.translation_model_id),
+            ('method', '=', 'write'),
+            ('res_id', '=', self.translation_record.id),
+        ], limit=1, order='id desc')
+        self.assertEqual(log.log_category, 'state_update')
+
+    def test_02_update_record_without_state(self):
+        self.translation_record.update({
+            'name': 'Test Translation Reord 2'
+        })
+        log = self.env['auditlog.log'].search([
+            ('model_id', '=', self.translation_model_id),
+            ('method', '=', 'write'),
+            ('res_id', '=', self.translation_record.id),
+        ], limit=1, order='id desc')
+        self.assertEqual(log.log_category, 'fields_update')
+
+    def test_03_unlink_record(self):
+        self.translation_record.unlink()
+        log = self.env['auditlog.log'].search([
+            ('model_id', '=', self.translation_model_id),
+            ('method', '=', 'unlink'),
+            ('res_id', '=', self.translation_record.id),
+        ], limit=1, order='id desc')
+        self.assertEqual(log.log_category, 'other')
