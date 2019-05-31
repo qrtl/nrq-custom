78e5ab7dee94014d8648174bdf8a8583ab636940
Tim <tl@quartile.co>
2019-05-31 11:08:01 +0800

    [ADD] auth_signup_nrq (#154)
    * [ADD] auth_signup_nrq
    
    * [IMP] Remove translations

---
+++ auth_signup_nrq/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Adjustments on Signup Functions",
+    "description": """
+    """,
+    "version": "10.0.1.0.0",
+    "category": "Extra Tools",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "auth_signup",
+    ],
+    "data": [
+        'views/res_users_views.xml',
+    ],
+}
