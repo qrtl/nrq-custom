112496610fb6241934f3d5c42fb05cfc5fd74ffd
Tim <tl@quartile.co>
2019-06-14 15:50:52 +0800

    [IMP] Add sudo to hr.holidays methods that access employee's fields (#158)
    * [IMP] Add sudo to methods that access employee's fields
    
    * [FIX] Remove print commands
    
    * [IMP] Add unit test to read hr.holidays

--- hr_employee_nrq/tests/test_hr_employee_nrq.py
+++ hr_employee_nrq/tests/test_hr_employee_nrq.py
@@ -35,6 +35,24 @@ class HrEmployeeNrq(common.TransactionCase):
                 self.env.ref('hr.group_hr_manager').id
             ])]
         ))
+        # Create hr holiday manager user
+        self.holiday_manager_user = self.env['res.users'].create(dict(
+            name="Holiday Manager",
+            company_id=self.env.ref('base.main_company').id,
+            login="hm",
+            password="hm",
+            email="homanager@yourcompany.example.com",
+            groups_id=[(6, 0, [self.env.ref('hr_holidays.group_hr_holidays_manager').id])]
+        ))
+        # Create hr holiday record
+        self.hr_holidays = self.env['hr.holidays'].create(dict(
+            employee_id=self.test_employee.id,
+            name="Test Holiday Record",
+            holiday_status_id=self.env['hr.holidays.status'].search([('limit', '=', False)])[0].id,
+            type='add',
+            holiday_type='employee',
+            number_of_days_temp=1
+        ))
 
     def test_00_officer_access_active_employee(self):
         self.test_employee.sudo().update({
@@ -60,3 +78,15 @@ class HrEmployeeNrq(common.TransactionCase):
             'active': False,
         })
         self.test_employee.sudo(self.manager_user.id).read()
+
+    def test_04_holiday_manager_access_active_hr_holdays_record(self):
+        self.test_employee.sudo().update({
+            'active': True,
+        })
+        self.hr_holidays.sudo(self.holiday_manager_user.id).read()
+
+    def test_05_holiday_manager_access_inactive_hr_holdays_record(self):
+        self.test_employee.sudo().update({
+            'active': False,
+        })
+        self.hr_holidays.sudo(self.holiday_manager_user.id).read()
