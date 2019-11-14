2eaebcf76b832f5e35cd428f340d6f976f4c307c
Tim <tl@quartile.co>
2019-11-14 22:07:06 +0800

    [IMP] Add 'Updated Manually Reason' (#163)

--- hr_attendance_timesheet_adj/__manifest__.py
+++ hr_attendance_timesheet_adj/__manifest__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 Quartile Limited
+# Copyright 2018-2019 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Adjustments on Attendance Functions',
     'summary': '',
     'description': '''
     ''',
-    'version': '10.0.1.2.0',
+    'version': '10.0.1.3.0',
     'category': 'HR',
     'website': 'https://www.quartile.co/',
     'author': 'Quartile Limited',
