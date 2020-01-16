3603d2c92329884bda27fbd78e62bc0439c9103f
Tim <tl@quartile.co>
2020-01-16 13:46:28 +0800

    [FIX] Undefine timedelta and HOURS_PER_DAY (#166)

--- hr_employee_nrq/models/hr_holidays.py
+++ hr_employee_nrq/models/hr_holidays.py
@@ -3,6 +3,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import models, fields, api, _
+from odoo.addons.hr_holidays.models.hr_holidays import HOURS_PER_DAY
+from datetime import timedelta
 
 
 class HrHolidays(models.Model):
