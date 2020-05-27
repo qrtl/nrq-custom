3da6088feb860eb99f04ee7655dcb8915a612efd
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-05-27 13:21:35 +0530

    [IMP]Improved Code

--- hr_timesheet_sheet_worked_time/models/hr_timesheet_sheet_sheet.py
+++ hr_timesheet_sheet_worked_time/models/hr_timesheet_sheet_sheet.py
@@ -59,11 +59,13 @@ class HrTimesheetSheet(models.Model):
 
     @api.depends('standard_work_hours', 'holiday_hours')
     def _compute_expected_work_hours(self):
-        self.expected_work_hours = self.standard_work_hours - self.holiday_hours
+        for sheet in self:
+            sheet.expected_work_hours = sheet.standard_work_hours - sheet.holiday_hours
 
     @api.depends('expected_work_hours', 'total_timesheet')
     def _compute_overtime_hours(self):
-        self.overtime_hours = self.total_timesheet - self.expected_work_hours
+        for sheet in self:
+            sheet.overtime_hours = sheet.total_timesheet - sheet.expected_work_hours
 
     @api.depends(
         'employee_id.calendar_id',
@@ -73,23 +75,21 @@ class HrTimesheetSheet(models.Model):
     def _compute_standard_work_hours(self):
         for sheet in self:
             today_date = fields.Datetime.to_string(datetime.now().date())
-            if sheet.date_from < today_date < sheet.date_to\
-                    or sheet.date_to < today_date:
-                date_from = datetime.strptime(str(
-                    sheet.date_from), DEFAULT_SERVER_DATE_FORMAT).date()
-                date_to = datetime.strptime(str(
-                    sheet.date_to), DEFAULT_SERVER_DATE_FORMAT).date()
-                day_count = (datetime.now().date() - date_from).days
-                if sheet.date_to < today_date:
-                    day_count = (date_to - date_from).days
-                total_time = 0.0
-                for single_date in (
-                        date_from + timedelta(n)
-                        for n in range(day_count + 1)):
-                    attendance_ids = \
-                        sheet.employee_id.calendar_id.attendance_ids.filtered(
-                            lambda attendance: int(
-                                attendance.dayofweek) == single_date.weekday())
-                    for attendance in attendance_ids:
-                        total_time += attendance.hour_from - attendance.hour_to
-                sheet.standard_work_hours = abs(total_time)
+            date_from = datetime.strptime(str(
+                sheet.date_from), DEFAULT_SERVER_DATE_FORMAT).date()
+            date_to = datetime.strptime(str(
+                sheet.date_to), DEFAULT_SERVER_DATE_FORMAT).date()
+            day_count = (datetime.now().date() - date_from).days
+            if sheet.date_to < today_date:
+                day_count = (date_to - date_from).days
+            total_time = 0.0
+            for single_date in (
+                    date_from + timedelta(n)
+                    for n in range(day_count + 1)):
+                attendance_ids = \
+                    sheet.employee_id.calendar_id.attendance_ids.filtered(
+                        lambda attendance: int(
+                            attendance.dayofweek) == single_date.weekday())
+                for attendance in attendance_ids:
+                    total_time += attendance.hour_from - attendance.hour_to
+            sheet.standard_work_hours = abs(total_time)
