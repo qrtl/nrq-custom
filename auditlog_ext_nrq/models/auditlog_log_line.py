ff43eebcc02ccbdfba2cf6cfa90b5a6e3d64c3a2
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-06-03 11:19:42 +0530

    [IMP]Improved Code

--- auditlog_ext_nrq/models/auditlog_log_line.py
+++ auditlog_ext_nrq/models/auditlog_log_line.py
@@ -2,6 +2,7 @@
 # Copyright 2020 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
+import pytz
 from odoo import api, fields, models
 
 
@@ -13,21 +14,24 @@ class AuditlogLogLine(models.Model):
         if vals.get('new_value_text') or vals.get('old_value_text'):
             old_value_text, new_value_text = vals.get(
                 'old_value_text'), vals.get('new_value_text')
+            timezone = pytz.timezone('Japan')
             try:
+                new_value_text = pytz.UTC.localize(
+                    fields.Datetime.from_string(new_value_text))
                 new_value_text =\
-                    fields.Datetime.to_string(
-                        fields.Datetime.context_timestamp(
-                            self, fields.Datetime.from_string(new_value_text)))
+                    fields.Datetime.to_string(new_value_text.replace(
+                    tzinfo=pytz.timezone('UTC')).astimezone(timezone))
                 vals.update({
                     'new_value_text': new_value_text
                 })
             except Exception:
                 pass
             try:
+                old_value_text = pytz.UTC.localize(
+                    fields.Datetime.from_string(old_value_text))
                 old_value_text =\
-                    fields.Datetime.to_string(
-                        fields.Datetime.context_timestamp(
-                            self, fields.Datetime.from_string(old_value_text)))
+                    fields.Datetime.to_string(old_value_text.replace(
+                    tzinfo=pytz.timezone('UTC')).astimezone(timezone))
                 vals.update({
                     'old_value_text': old_value_text
                 })
