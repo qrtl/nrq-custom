e528c727a4116ad8e7ede62046b5a09dc2993a13
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-06-03 12:19:11 +0530

    [IMP]Improved license

--- auditlog_ext_nrq/__manifest__.py
+++ auditlog_ext_nrq/__manifest__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019-2020 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Audit Log Extension NRQ",
     'version': "10.0.1.0.1",
-    'license': 'LGPL-3',
+    'license': 'AGPL-3',
     'author': 'Quartile Limited',
     'website': 'http://www.quartile.co/',
     'category': "Tools",
