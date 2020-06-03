3419bd37c6bf89209c900da1f2ce208a9a27bec1
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-06-03 14:57:02 +0530

    [10.0][T889][ADD] Added hr_timesheet_sheet_worked_time Module (#181)

---
+++ hr_timesheet_sheet_work_hours_summary/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Timesheet Working Hours Summary',
+    'summary': '',
+    'version': '10.0.1.0.0',
+    'category': 'HR',
+    'website': 'https://www.quartile.co/',
+    'author': 'Quartile Limited',
+    'license': 'AGPL-3',
+    'depends': [
+        'hr_public_holidays',
+        'hr_timesheet_attendance',
+    ],
+    'data': [
+        'views/hr_timesheet_sheet_sheet_views.xml',
+    ],
+    'application': False,
+    'installable': True,
+}
---
+++ hr_timesheet_sheet_work_hours_summary/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module does the following:
+
+* Employee check the expected hours within Timesheet Period.
+* Employee can check their overtime hours within Timesheet Period.
