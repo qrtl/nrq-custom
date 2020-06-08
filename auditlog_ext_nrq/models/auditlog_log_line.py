f0c011204ef304f907097b45f8476619cafe468b
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-06-08 13:40:23 +0530

    [IMP]Optimized code

--- auditlog_ext_nrq/models/auditlog_log_line.py
+++ auditlog_ext_nrq/models/auditlog_log_line.py
@@ -8,30 +8,23 @@ from odoo import SUPERUSER_ID, api, fields, models
 class AuditlogLogLine(models.Model):
     _inherit = 'auditlog.log.line'
 
+    def convert_time_to_users_timezone(self, value, tz):
+        try:
+            value = fields.Datetime.to_string(
+                fields.Datetime.context_timestamp(
+                    self.with_context(tz=tz),
+                    fields.Datetime.from_string(value)))
+            return value
+        except:
+            return value
+
     @api.model
     def create(self, vals):
-        if vals.get('new_value_text') or vals.get('old_value_text'):
-            old_value_text, new_value_text = vals.get(
-                'old_value_text'), vals.get('new_value_text')
-            tz = self.env['res.users'].sudo().browse(SUPERUSER_ID).tz or 'UTC'
-            try:
-                new_value_text = fields.Datetime.to_string(
-                    fields.Datetime.context_timestamp(
-                        self.with_context(tz=tz),
-                        fields.Datetime.from_string(new_value_text)))
-                vals.update({
-                    'new_value_text': new_value_text
-                })
-            except Exception:
-                pass
-            try:
-                old_value_text = fields.Datetime.to_string(
-                    fields.Datetime.context_timestamp(
-                        self.with_context(tz=tz),
-                        fields.Datetime.from_string(old_value_text)))
-                vals.update({
-                    'old_value_text': old_value_text
-                })
-            except Exception:
-                pass
+        tz = self.env['res.users'].sudo().browse(SUPERUSER_ID).tz or 'UTC'
+        if vals.get('old_value_text'):
+            vals.update({'old_value_text': self.convert_time_to_users_timezone(
+                vals.get('old_value_text'), tz)})
+        if vals.get('new_value_text'):
+            vals.update({'new_value_text': self.convert_time_to_users_timezone(
+                vals.get('new_value_text'), tz)})
         return super(AuditlogLogLine, self).create(vals)
