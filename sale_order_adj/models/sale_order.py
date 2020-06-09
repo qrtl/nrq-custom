125cfa846d6be4b35fe3b29c9a1b6eaa15a05349
Tim <tl@quartile.co>
2020-06-09 20:20:28 +0800

    [1062][IMP] Add gross_profit_margin_input as required field (#187)

--- sale_order_adj/models/sale_order.py
+++ sale_order_adj/models/sale_order.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-# Copyright 2018 Quartile Limited
+# Copyright 2018-2020 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields, api
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class SaleOrder(models.Model):
@@ -12,6 +13,18 @@ class SaleOrder(models.Model):
         related='project_project_id.user_id',
         string='Project Manager',
     )
+    gross_profit_margin_input = fields.Char(
+        string='Gross Profit Margin',
+        required=True,
+    )
     gross_profit_margin = fields.Float(
         string='Gross Profit Margin',
     )
+
+    @api.onchange('gross_profit_margin_input')
+    def _onchange_gross_profit_margin_input(self):
+        if self.gross_profit_margin_input:
+            try:
+                self.gross_profit_margin = float(self.gross_profit_margin_input)
+            except Exception:
+                raise UserError(_("The gross_profit_margin must be a number."))
