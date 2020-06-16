b7628fb24a699c20556c3a886d55287725e63e85
Yogesh Mahera <yogesh.mahera.serpentcs@gmail.com>
2020-06-16 09:03:49 +0530

    [889][IMP] Improve test cases, fix access errors and add float_time widget (#188)

--- hr_timesheet_sheet_work_hours_summary/__manifest__.py
+++ hr_timesheet_sheet_work_hours_summary/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Timesheet Working Hours Summary',
     'summary': '',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'HR',
     'website': 'https://www.quartile.co/',
     'author': 'Quartile Limited',
@@ -14,6 +14,7 @@
         'hr_timesheet_attendance',
     ],
     'data': [
+        'security/ir.model.access.csv',
         'views/hr_timesheet_sheet_sheet_views.xml',
     ],
     'application': False,
