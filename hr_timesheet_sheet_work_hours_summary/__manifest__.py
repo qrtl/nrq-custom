c8efc39a9ffe1a1e5c761e999a1d9a497c4715f3
tslai <tl@quartile.co>
2020-06-16 11:27:57 +0800

    [IMP] Add add float_time widget to hours fields

--- hr_timesheet_sheet_work_hours_summary/__manifest__.py
+++ hr_timesheet_sheet_work_hours_summary/__manifest__.py
@@ -4,7 +4,7 @@
 {
     'name': 'Timesheet Working Hours Summary',
     'summary': '',
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.0.1',
     'category': 'HR',
     'website': 'https://www.quartile.co/',
     'author': 'Quartile Limited',
