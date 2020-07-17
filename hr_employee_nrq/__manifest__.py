f42ce29b1d6c434c1e4abc2e32077b7641f4bb58
tslai <tl@quartile.co>
2020-07-17 17:17:03 +0800

    [IMP] Grant res.country write access to employee

--- hr_employee_nrq/__manifest__.py
+++ hr_employee_nrq/__manifest__.py
@@ -3,7 +3,7 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Adjustments on HR Employee Functions",
-    "version": "10.0.1.2.0",
+    "version": "10.0.1.2.1",
     "category": "HR",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
@@ -17,6 +17,7 @@
     ],
     "data": [
         'security/hr_security.xml',
+        'security/ir.model.access.csv',
         'views/hr_employee_views.xml',
     ],
 }
