247f0e3e1833e4c468f74b5f6f50125363ca5c43
Tim <tl@quartile.co>
2020-09-22 17:47:37 +0800

    [1137][ADD] hr_holidays_compute_days_adj (#195)

---
+++ hr_holidays_compute_days_adj/models/hr_holidays.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class HrHolidays(models.Model):
+    _inherit = 'hr.holidays'
+
+    from_full_day = fields.Boolean(
+        default=True
+    )
+    to_full_day = fields.Boolean(
+        default=True
+    )
+    # Set default to avoid null value if user does not create the leave record
+    # from calendar value
+    date_from = fields.Datetime(
+        default=fields.Datetime.now
+    )
+    date_to = fields.Datetime(
+        default=fields.Datetime.now
+    )
---
+++ hr_holidays_compute_days_adj/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module does the following:
+
+- Makes from_full_day and to_full_day options as default.
+- Sets default values to date_from and date_to.
+- Adjusts Leave Request form view.
