34d4e5caea48e3e888879e85bd408ac34a44b26c
Tim <tl@quartile.co>
2020-10-09 09:55:03 +0800

    [1284][ADD] user_activity_log (#196)

---
+++ user_activity_log/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': "User Activity Log",
+    'category': 'Extra Tools',
+    'version': '10.0.1.0.0',
+    'author': 'Quartile Limited, Aktiv Software',
+    'website': 'https://www.quartile.co',
+    'license': "AGPL-3",
+    'depends': ['base'],
+    'data': [
+        'data/ir_cron.xml',
+        'security/ir.model.access.csv',
+        'views/user_activity_log_views.xml',
+        'views/res_users_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
