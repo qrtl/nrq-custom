d5cb7ca44bb9788d31003530c69a1dfd8819f0ea
Ryoko <ryoko@quartile.co>
2020-11-13 14:17:35 +0800

    [IMP]auditlog_ext_nrq: pre-commit, README

--- auditlog_ext_nrq/__manifest__.py
+++ auditlog_ext_nrq/__manifest__.py
@@ -2,18 +2,14 @@
 # Copyright 2019-2020 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': "Audit Log Extension NRQ",
-    'version': "10.0.1.0.2",
-    'license': 'AGPL-3',
-    'author': 'Quartile Limited',
-    'website': 'http://www.quartile.co/',
-    'category': "Tools",
-    'depends': [
-        'auditlog',
-    ],
-    'data': [
-        'views/auditlog_log_views.xml',
-    ],
-    'application': True,
-    'installable': True,
+    "name": "Audit Log Extension NRQ",
+    "version": "10.0.1.0.2",
+    "license": "AGPL-3",
+    "author": "Quartile Limited",
+    "website": "http://www.quartile.co/",
+    "category": "Tools",
+    "depends": ["auditlog"],
+    "data": ["views/auditlog_log_views.xml"],
+    "application": True,
+    "installable": True,
 }
--- auditlog_ext_nrq/models/auditlog_log.py
+++ auditlog_ext_nrq/models/auditlog_log.py
@@ -6,28 +6,33 @@ from odoo import api, fields, models
 
 
 class AuditlogLog(models.Model):
-    _inherit = 'auditlog.log'
+    _inherit = "auditlog.log"
 
-    log_category = fields.Selection([
-        ('state_update', 'Submitted/Set back to Draft'),
-        ('fields_update', 'Fields Update'),
-        ('create', 'Record Created'),
-        ('unlink', 'Record Unlinked'),
-        ('other', 'N/A')],
-        string='Log Category',
-        compute='_get_log_category',
+    log_category = fields.Selection(
+        [
+            ("state_update", "Submitted/Set back to Draft"),
+            ("fields_update", "Fields Update"),
+            ("create", "Record Created"),
+            ("unlink", "Record Unlinked"),
+            ("other", "N/A"),
+        ],
+        string="Log Category",
+        compute="_compute_get_log_category",
         store=True,
     )
 
     @api.multi
-    @api.depends('line_ids')
-    def _get_log_category(self):
+    @api.depends("line_ids")
+    def _compute_get_log_category(self):
         for log in self:
-            log.log_category = 'other'
-            if log.method == 'write' and log.line_ids:
-                log.log_category = 'state_update' if log.line_ids.filtered(
-                    lambda r: r.field_name == 'state') else 'fields_update'
-            elif log.method == 'create':
-                log.log_category = 'create'
-            elif log.method == 'unlink':
-                log.log_category = 'unlink'
+            log.log_category = "other"
+            if log.method == "write" and log.line_ids:
+                log.log_category = (
+                    "state_update"
+                    if log.line_ids.filtered(lambda r: r.field_name == "state")
+                    else "fields_update"
+                )
+            elif log.method == "create":
+                log.log_category = "create"
+            elif log.method == "unlink":
+                log.log_category = "unlink"
