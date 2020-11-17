072660b97116d6ebcffa7be779f575ce539ca109
Ryoko <ryoko@quartile.co>
2020-11-17 13:33:52 +0800

    [FIX] Reflect the review

--- hr_timesheet_sheet_view_adj/__manifest__.py
+++ hr_timesheet_sheet_view_adj/__manifest__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2020 Quartile Limited
+# Copyright 2020 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Adjustments on Timesheet view filter",
