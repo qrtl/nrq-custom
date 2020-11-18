38cfc8e6dce2acf34683af2a3917db334fff6b22
tslai <tl@quartile.co>
2020-11-18 21:34:05 +0800

    [ADD] hr_weekly_timesheet_widget_adj

---
+++ hr_weekly_timesheet_widget_adj/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    'name': 'Weekly Timesheet Widget Adjustments',
+    'version': '10.0.1.0.0',
+    'category': 'HR',
+    'website': 'https://www.quartile.co/',
+    'author': 'Quartile Limited',
+    'license': 'LGPL-3',
+    'depends': [
+        'hr_timesheet_sheet'
+    ],
+    'data': [
+        'views/assets.xml',
+    ],
+    'application': False,
+    'installable': True,
+}
