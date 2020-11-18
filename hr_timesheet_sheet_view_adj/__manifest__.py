d3c8b537b8de63631c08d8fc37a4657ae7a908f6
Ryoko Tsuda <70553601+Ryoko04@users.noreply.github.com>
2020-11-18 14:27:22 +0900

    [1435][ADD]hr_timesheet_filter_adj (#210)
    * [ADD]hr_timesheet_filter_adj
    * [FIX] Reflect the review
    * [FIX] Reflect the reviews 2nd

---
+++ hr_timesheet_sheet_view_adj/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Adjustments on Timesheet views",
+    "version": "10.0.1.0.0",
+    "category": "HR",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["hr_timesheet_sheet"],
+    "data": ["views/hr_timesheet_sheet_views.xml"],
+}
