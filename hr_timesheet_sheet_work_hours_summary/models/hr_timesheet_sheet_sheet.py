5f1731e3d8c34dcecf241649e3a2dab7884db53e
tslai <tl@quartile.co>
2021-01-05 10:29:31 +0800

    [FIX] Year domain in search holiday records

--- hr_timesheet_sheet_work_hours_summary/models/hr_timesheet_sheet_sheet.py
+++ hr_timesheet_sheet_work_hours_summary/models/hr_timesheet_sheet_sheet.py
@@ -65,10 +65,12 @@ class HrTimesheetSheet(models.Model):
 
     def get_holiday_hours(self, date):
         date_to = date if self.date_to > date else self.date_to
+        from_year = fields.Date.from_string(self.date_from).year
+        to_year = fields.Date.from_string(self.date_to).year
         public_holiday_line_ids = self.env['hr.holidays.public.line'].search([
             ('date', '>=', self.date_from),
             ('date', '<=', date_to),
-            ('year_id.year', '=', datetime.now().year),
+            ('year_id.year', 'in', (from_year, to_year)),
         ])
         holiday_hours = 0.0
         for line in public_holiday_line_ids:
