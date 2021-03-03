99e2946bf05da614d8056700523cbee47253bcf4
tslai <tl@quartile.co>
2021-03-03 17:34:07 +0800

    [IMP] hr_expense_adj

--- hr_expense_adj/__manifest__.py
+++ hr_expense_adj/__manifest__.py
@@ -1,27 +1,18 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2018 Quartile Limited
+# Copyright 2017-2021 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Adjustments on Expense Functions",
-    "summary": "",
-    "description": """
-* Hides "Submit to Manager" button and Account field in expense form
-* Proposes default values in expense report summary field
-* Adds 'draft' state to expense report
-    """,
-    "version": "10.0.1.4.2",
+    "version": "10.0.1.4.3",
     "category": "HR",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
     "license": "AGPL-3",
     "installable": True,
-    "depends": [
-        "hr_expense",
-        "ir_sequence_range_month",
-    ],
+    "depends": ["hr_expense", "ir_sequence_range_month"],
     "data": [
-        'data/expense_data.xml',
-        'views/hr_expense_views.xml',
-        'views/hr_expense_sheet_views.xml',
+        "data/expense_data.xml",
+        "views/hr_expense_views.xml",
+        "views/hr_expense_sheet_views.xml",
     ],
 }
--- hr_expense_adj/models/hr_expense_sheet.py
+++ hr_expense_adj/models/hr_expense_sheet.py
@@ -6,75 +6,69 @@ from odoo import api, fields, models
 
 
 class HrExpenseSheet(models.Model):
-    _inherit = 'hr.expense.sheet'
+    _inherit = "hr.expense.sheet"
 
     def _default_name(self):
         today = fields.Date.context_today(self)
-        y, m, d = today.split('-')
-        return y + '.' + m
+        y, m, d = today.split("-")
+        return y + "." + m
 
-    name = fields.Char(
-        default=_default_name,
-    )
+    name = fields.Char(default=_default_name,)
     # add 'draft' state, and change default to draft
     state = fields.Selection(
-        [('draft', 'Draft'),
-         ('submit', 'Submitted'),
-         ('approve', 'Approved'),
-         ('post', 'Posted'),
-         ('done', 'Paid'),
-         ('cancel', 'Refused')],
-        default='draft',
+        [
+            ("draft", "Draft"),
+            ("submit", "Submitted"),
+            ("approve", "Approved"),
+            ("post", "Posted"),
+            ("done", "Paid"),
+            ("cancel", "Refused"),
+        ],
+        default="draft",
     )
     # add 'draft' state to cancel readonly for following fields
     employee_id = fields.Many2one(
-        states={'draft': [('readonly', False)],
-                'submit': [('readonly', False)]}
+        states={"draft": [("readonly", False)], "submit": [("readonly", False)]}
     )
     responsible_id = fields.Many2one(
-        states={'draft': [('readonly', False)],
-                'submit': [('readonly', False)]}
+        states={"draft": [("readonly", False)], "submit": [("readonly", False)]}
     )
     company_id = fields.Many2one(
-        states={'draft': [('readonly', False)],
-                'submit': [('readonly', False)]}
+        states={"draft": [("readonly", False)], "submit": [("readonly", False)]}
     )
     currency_id = fields.Many2one(
-        states={'draft': [('readonly', False)],
-                'submit': [('readonly', False)]}
+        states={"draft": [("readonly", False)], "submit": [("readonly", False)]}
     )
     # Overwrite default field definition
-    expense_line_ids = fields.One2many('hr.expense', 'sheet_id',
-                                       string='Expense Lines',
-                                       states={'done': [('readonly', True)],
-                                               'post': [('readonly', True)]},
-                                       copy=False)
+    expense_line_ids = fields.One2many(
+        "hr.expense",
+        "sheet_id",
+        string="Expense Lines",
+        states={"done": [("readonly", True)], "post": [("readonly", True)]},
+        copy=False,
+    )
     code = fields.Char(
-        related='employee_id.code',
-        string='Code',
-        store=True,
-        readonly=True,
+        related="employee_id.code", string="Code", store=True, readonly=True,
     )
 
     def _assign_number(self, line):
-        context = {'ir_sequence_date': line.date}
-        line.number = self.env['ir.sequence'].with_context(context). \
-            next_by_code('hr.expense')
+        context = {"ir_sequence_date": line.date}
+        line.number = (
+            self.env["ir.sequence"].with_context(context).next_by_code("hr.expense")
+        )
 
     @api.model
     def create(self, vals):
         sheet = super(HrExpenseSheet, self).create(vals)
-        for line in sheet.expense_line_ids.sorted(
-                key=lambda x: (x.date, x.id)):
+        for line in sheet.expense_line_ids.sorted(key=lambda x: (x.date, x.id)):
             self._assign_number(line)
         return sheet
 
     @api.multi
     def write(self, vals):
         res = super(HrExpenseSheet, self).write(vals)
-        if self.state in ['draft', 'submit']:
-            for line in self.expense_line_ids.sorted(
-                    key=lambda x: (x.date, x.id)):
+        if self.state in ["draft", "submit"]:
+            for line in self.expense_line_ids.sorted(key=lambda x: (x.date, x.id)):
                 if not line.number:
                     self._assign_number(line)
         return res
@@ -87,14 +81,14 @@ class HrExpenseSheet(models.Model):
 
     @api.multi
     def action_submit(self):
-        return self.write({'state': 'submit'})
+        return self.write({"state": "submit"})
 
     @api.multi
     def action_cancel_submission(self):
-        return self.write({'state': 'draft'})
+        return self.write({"state": "draft"})
 
     @api.multi
     def reset_expense_sheets(self):
         # override the standard method
         # return self.write({'state': 'submit'})
-        return self.write({'state': 'draft'})
+        return self.write({"state": "draft"})
