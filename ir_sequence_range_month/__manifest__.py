d370ef9d52e34d56074a563a94294f0088dd8721
tslai <tl@quartile.co>
2021-03-03 17:48:47 +0800

    [IMP] ir_sequence_range_month

--- ir_sequence_range_month/__manifest__.py
+++ ir_sequence_range_month/__manifest__.py
@@ -1,35 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Quartile Limited
+# Copyright 2017-2021 Quartile Limited
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
     "name": "Auto-create monthly date ranges for sequence",
-    "summary": "",
-    "description": """
-* Odoo's sequence engine by defauld does not support refreshing 'next number'
-on monthly basis.  This module adds thel logic to support it.
-    """,
     "version": "10.0.1.0.0",
     "category": "Technical Settings",
-    "website": "https://www.odoo-asia.com/",
+    "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
     "license": "LGPL-3",
     "application": False,
     "installable": True,
-    "pre_init_hook": "",
-    "post_init_hook": "",
-    "post_load": "",
-    "uninstall_hook": "",
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
-    "depends": [
-        "base",
-    ],
-    "data": [
-    ],
-    "demo": [
-    ],
-    "qweb": [
-    ]
+    "depends": ["base"],
 }
