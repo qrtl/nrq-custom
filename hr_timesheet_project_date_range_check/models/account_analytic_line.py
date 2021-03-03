a88a1e6aa78a039c035dccb4963da57af735d000
tslai <tl@quartile.co>
2021-03-03 17:44:28 +0800

    [IMP] hr_timesheet_project_date_range_check

--- hr_timesheet_project_date_range_check/models/account_analytic_line.py
+++ hr_timesheet_project_date_range_check/models/account_analytic_line.py
@@ -7,15 +7,20 @@ from odoo.exceptions import ValidationError
 
 
 class AccountAnalyticLine(models.Model):
-    _inherit = 'account.analytic.line'
+    _inherit = "account.analytic.line"
 
-    @api.one
-    @api.constrains('project_id', 'date')
+    @api.constrains("project_id", "date")
     def check_project_date(self):
         if self.project_id:
-            if self.project_id.date_start and self.date < \
-                    self.project_id.date_start or self.project_id.date and \
-                    self.date > self.project_id.date:
-                raise ValidationError(_(
-                    'You cannot have an entry that the date is out of the '
-                    'project date range'))
+            if (
+                self.project_id.date_start
+                and self.date < self.project_id.date_start
+                or self.project_id.date
+                and self.date > self.project_id.date
+            ):
+                raise ValidationError(
+                    _(
+                        "You cannot have an entry that the date is out of the "
+                        "project date range"
+                    )
+                )
