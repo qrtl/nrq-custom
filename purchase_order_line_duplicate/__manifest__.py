3054edacdf52d6dfba0e61e05c9d93e757165428
tslai <tl@quartile.co>
2021-03-03 18:05:35 +0800

    [IMP] purchase_order_line_duplicate

--- purchase_order_line_duplicate/__manifest__.py
+++ purchase_order_line_duplicate/__manifest__.py
@@ -3,11 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Duplicate Purchase Order Lines",
-    "summary": "",
-    "description": """
-- Add "Duplicate Purchase Lines" button to sale order form view that will
-duplicate specific order line in the purchase order.
-    """,
     "version": "10.0.1.0.0",
     "category": "Purchase",
     "website": "https://www.quartile.co/",
@@ -15,10 +10,6 @@ duplicate specific order line in the purchase order.
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        "purchase",
-    ],
-    "data": [
-        'views/purchase_order_views.xml',
-    ],
+    "depends": ["purchase"],
+    "data": ["views/purchase_order_views.xml"],
 }
---
+++ purchase_order_line_duplicate/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module adds "Duplicate Purchase Lines" button to sale order form view that will duplicate specific order line in the purchase order.
