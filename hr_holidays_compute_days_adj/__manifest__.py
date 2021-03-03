bca301964687253fa4f2534d0669bddfe6043110
tslai <tl@quartile.co>
2021-03-03 17:37:48 +0800

    [IMP] hr_holidays_compute_days_adj

--- hr_holidays_compute_days_adj/__manifest__.py
+++ hr_holidays_compute_days_adj/__manifest__.py
@@ -2,19 +2,15 @@
 # Copyright 2020 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Employee Compute Leave Days Adjustments',
-    'summary': '',
-    'version': '10.0.1.0.0',
-    'category': 'HR',
-    'website': 'https://www.quartile.co/',
-    'author': 'Quartile Limited',
-    'license': 'AGPL-3',
-    'depends': [
-        'hr_holidays_compute_days',
-    ],
-    'data': [
-        'views/hr_holidays_views.xml'
-    ],
-    'application': False,
-    'installable': True,
+    "name": "Employee Compute Leave Days Adjustments",
+    "summary": "",
+    "version": "10.0.1.0.0",
+    "category": "HR",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "depends": ["hr_holidays_compute_days"],
+    "data": ["views/hr_holidays_views.xml"],
+    "application": False,
+    "installable": True,
 }
