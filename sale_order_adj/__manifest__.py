bb750064540d37eb9093062cc0148d7f6aa79e51
tslai <tl@quartile.co>
2021-03-03 21:23:46 +0800

    [IMP] sale_order_adj

--- sale_order_adj/__manifest__.py
+++ sale_order_adj/__manifest__.py
@@ -3,12 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Adjustments on Sales Functions",
-    "summary": "",
-    "description": """
-- Show Project Manager in the list and form view
-- Show Untaxed Amount in the list view
-- Show Confirmation Date instead of order date in the list view
-    """,
     "version": "10.0.1.2.0",
     "category": "Sales",
     "website": "https://www.quartile.co/",
@@ -22,7 +16,5 @@
         "sale_quotation_approval",
         "account_analytic_profit_loss_analysis",
     ],
-    "data": [
-        'views/sale_order_views.xml',
-    ],
+    "data": ["views/sale_order_views.xml"],
 }
---
+++ sale_order_adj/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+This module does the following:
+
+- Show Project Manager in the list and form view
+- Show Untaxed Amount in the list view
+- Show Confirmation Date instead of order date in the list view
