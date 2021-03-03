fbc6d4d9420cf590878a64a87a322073913f20aa
tslai <tl@quartile.co>
2021-03-03 16:47:32 +0800

    [IMP] base_partner_number

--- base_partner_number/__manifest__.py
+++ base_partner_number/__manifest__.py
@@ -1,35 +1,15 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Rooms For (Hong Kong) Limited T/A OSCG
+# Copyright 2016-2021 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Partner Number',
+    "name": "Partner Number",
     "version": "10.0.1.0.0",
-    "summary": "",
-    'description': """
-            """,
-    'category': 'Base',
-    "website": "https://www.odoo-asia.com/",
-    "author": "Rooms For (Hong Kong) Limited T/A OSCG",
+    "category": "Base",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
     "license": "AGPL-3",
+    "depends": ["base"],
+    "data": ["data/res_partner_data.xml", "views/res_partner_views.xml"],
     "application": False,
     "installable": True,
-    "pre_init_hook": "",
-    "post_init_hook": "",
-    "post_load": "",
-    "uninstall_hook": "",
-    "external_dependencies": {
-        "python": [],
-        "bin": [],
-    },
-    'depends': [
-        'base',
-    ],
-    'data': [
-        'data/res_partner_data.xml',
-        'views/res_partner_views.xml',
-    ],
-    "demo": [
-    ],
-    "qweb": [
-    ]
 }
