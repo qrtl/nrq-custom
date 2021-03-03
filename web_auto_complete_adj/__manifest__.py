1fbbab0577deb225ca725c6f7b16b3feb252c5c5
tslai <tl@quartile.co>
2021-03-03 22:16:13 +0800

    [IMP] web_auto_complete_adj

--- web_auto_complete_adj/__manifest__.py
+++ web_auto_complete_adj/__manifest__.py
@@ -2,23 +2,13 @@
 # Copyright 2020 Quartile Limited
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    'name': 'Auto-Complete Search Patch',
-    'version': '10.0.1.0.0',
-    'author': 'Quartile Limited',
-    'website': 'https://www.quartile.co',
-    'category': 'Web',
-    'license': 'AGPL-3',
-    'summary': "",
-    'description': """
-- Applying the fix batch of fixing the update search
- view menu on click IME suggestion menu.
-https://github.com/odoo/odoo/commit/6610b5d8fffe567b2dd7e4f45815eb4643defe83
-    """,
-    'depends': [
-        'web'
-    ],
-    'data': [
-        'views/asset.xml',
-    ],
-    'installable': True,
+    "name": "Auto-Complete Search Patch",
+    "version": "10.0.1.0.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Web",
+    "license": "AGPL-3",
+    "depends": ["web"],
+    "data": ["views/asset.xml"],
+    "installable": True,
 }
---
+++ web_auto_complete_adj/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module applies the fix batch of fixing the update search view menu on click IME suggestion menu.
+https://github.com/odoo/odoo/commit/6610b5d8fffe567b2dd7e4f45815eb4643defe83
