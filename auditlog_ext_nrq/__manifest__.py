f072a063cf34713f831b3e17b0b1f52dc0bb61fb
tslai <tl@quartile.co>
2021-03-19 10:19:16 +0800

    [IMP] Add auditlog line view

--- auditlog_ext_nrq/__manifest__.py
+++ auditlog_ext_nrq/__manifest__.py
@@ -3,13 +3,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Audit Log Extension NRQ",
-    "version": "10.0.1.0.2",
+    "version": "10.0.1.0.3",
     "license": "AGPL-3",
     "author": "Quartile Limited",
     "website": "http://www.quartile.co/",
     "category": "Tools",
     "depends": ["auditlog"],
-    "data": ["views/auditlog_log_views.xml"],
+    "data": ["views/auditlog_log_views.xml", "views/auditlog_log_line_views.xml"],
     "application": True,
     "installable": True,
 }
