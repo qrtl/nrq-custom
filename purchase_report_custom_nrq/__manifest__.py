ee30e75e5566046f3827e78577540b9ca14908cf
TomohiroAbe-Quartile <tomohiro@quartile.co>
2021-11-09 14:11:51 +0800

    edit new file

--- purchase_report_custom_nrq/__manifest__.py
+++ purchase_report_custom_nrq/__manifest__.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
-# Copyright 2019 Quartile Limited
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# Copyright 2021 Quartile Limited
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 {
-    "name": "Duplicate Purchase Order Lines",
+    "name": "Purchase Document Custom",
     "version": "10.0.1.0.0",
     "category": "Purchase",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "application": False,
     "installable": True,
-    "depends": ["purchase_stock"],
+    "depends": ["report_purhcase_nrq"],
     "data": [
         "report/purchase_order_report_templates.xml",
         "views/purchase_order_views.xml"
