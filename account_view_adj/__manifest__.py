257df39eb2bcadb5af3bc8efdedae488e9e808da
kanda999 <kanda@quartile.co>
2022-06-21 17:47:15 +0800

    [ADD] account_invoice_view

---
+++ account_view_adj/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2019 Quartile Limited
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Account View Adj",
+    "summary": "",
+    "version": "10.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["account"],
+    "data": ["views/account_invoice_views.xml"],
+}
