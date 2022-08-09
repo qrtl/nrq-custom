1e286764252d3eabb8b9a8ef708873cd3a2310f7
kanda999 <kanda@quartile.co>
2022-08-09 10:08:00 +0800

    fix-translate=T

--- l10n_jp_hr_employee/models/res_company.py
+++ l10n_jp_hr_employee/models/res_company.py
@@ -9,4 +9,6 @@ class ResCompany(models.Model):
     _inherit = "res.company"
 
     appointment_letter_url = fields.Char("Letter of Appointment Link(" "Dependant)")
-    private_page_header_text = fields.Html("Private Page Header Text", sanitize=True)
+    private_page_header_text = fields.Html(
+        "Private Page Header Text", translate=True, sanitize=True
+    )
