05c9f96496093edd748a994f870b9df95106b1b3
kanda999 <kanda@quartile.co>
2022-08-12 12:08:38 +0800

    [ADD] account_invoice_partner_number

---
+++ account_invoice_partner_number/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Account Invoice Partner Number",
+    "version": "10.0.1.0.0",
+    "category": "Accounting",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": ["account","base_partner_number"],
+    "data": ["views/account_invoice_views.xml"],
+}
