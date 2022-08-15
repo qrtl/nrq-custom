776e25086a8d4d9988657f35f6a53daa3680fc9f
Yoshi Tashiro <tashiro@quartile.co>
2022-08-15 17:52:06 +0800

    [2546][ADD] hr_holidays_employee_manager

---
+++ hr_holidays_employee_manager/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "HR Holidays Employee Manager",
+    "version": "10.0.1.0.0",
+    "category": "HR",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co/",
+    "license": "LGPL-3",
+    "depends": ["hr_holidays"],
+    "data": ["views/hr_holidays_views.xml"],
+    "installable": True,
+}
