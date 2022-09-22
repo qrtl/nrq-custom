fcebebdaa8f1c1e65eb9ac3e9a1831a8c233b8a5
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2022-09-22 21:44:26 +0900

    [2623][ADD] partner_restrict_update (#252)

---
+++ partner_restrict_update/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner Restrict Update",
+    "version": "10.0.1.0.0",
+    "category": "Sales",
+    "website": "https://www.quartile.co/",
+    "author": "Quartile Limited",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["base"],
+}
---
+++ partner_restrict_update/readme/DESCRIPTION.rst
@@ -0,0 +1,4 @@
+This module limits the create/write right to the users with the 'Contact Creation' group
+(base.group_partner_manager).
+
+As an exception, users are able to update the partners that link to themselves.
