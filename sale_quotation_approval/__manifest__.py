c8ec4b9c0dc94f2783f9089a1ec8b3c869a3184a
Yoshi Tashiro <tashiro@quartile.co>
2022-09-22 22:09:38 +0800

    [2623][IMP] sale_quotation_approval

--- sale_quotation_approval/__manifest__.py
+++ sale_quotation_approval/__manifest__.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017-2018 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# Copyright 2017-2022 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 {
     "name": "Quotation Approval",
     "summary": "",
-    "version": "10.0.1.3.0",
+    "version": "10.0.1.3.1",
     "category": "Sales",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
     "license": "LGPL-3",
-    "application": False,
     "installable": True,
     "depends": ["sale", "report_sale_nrq"],
     "data": [
