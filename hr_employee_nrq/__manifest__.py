d10dc01b716856011fbf6388a06762346dda2368
Aungkokolin1997 <lin@quartile.co>
2023-09-25 11:32:41 +0630

    [FIX] hr_employee_nrq: change license to AGPL

--- hr_employee_nrq/__manifest__.py
+++ hr_employee_nrq/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2019-2020 Quartile Limited
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Adjustments on HR Employee Functions",
     "version": "10.0.1.2.1",
     "category": "HR",
     "website": "https://www.quartile.co/",
     "author": "Quartile Limited",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "installable": True,
     "depends": [
         "hr_contract",
