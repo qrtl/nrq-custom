976e6cdeda34598f16d7aff56b34eff444187f6d
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2023-11-20 09:12:55 +0630

    [3767][UPD] remove (.ALT) reports (#263)
    * [UPD] remove (.ALT) reports

--- report_common_nrq/models/res_company.py
+++ report_common_nrq/models/res_company.py
@@ -9,17 +9,10 @@ class Company(models.Model):
     _inherit = "res.company"
 
     company_name = fields.Char("Company Name for Reports", translate=True,)
-    company_name_alt = fields.Char("Company Name for Reports (Alt.)", translate=True,)
     company_address = fields.Text("Address for Reports", translate=True,)
-    company_address_alt = fields.Text("Address for Reports (Alt.)", translate=True,)
     company_phone = fields.Char("Phone for Reports", translate=True,)
-    company_phone_alt = fields.Char("Phone for Reports (Alt.)", translate=True,)
     company_fax = fields.Char("Fax for Reports", translate=True,)
-    company_fax_alt = fields.Char("Fax for Reports (Alt.)", translate=True,)
     company_website = fields.Char("Website for Reports",)
-    company_website_alt = fields.Char("Website for Reports (Alt.)",)
     bank_details = fields.Text(string="Bank Details", translate=True,)
-    bank_details_alt = fields.Text(string="Bank Details (Alt.)", translate=True,)
     chop = fields.Binary("Company Chop Image", attachment=True,)
-    chop_alt = fields.Binary("Company Chop Image (Alt.)", attachment=True,)
     invoice_remarks = fields.Text(string="Invoice Remarks",)
