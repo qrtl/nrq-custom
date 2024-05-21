ee688ce229cdb1694ecc5d52f964e9cdb7625d14
Aungkokolin1997 <aungkokolin23397@gmail.com>
2024-05-21 16:12:59 +0800

    adj

--- l10n_jp_hr_employee/models/res_company.py
+++ l10n_jp_hr_employee/models/res_company.py
@@ -11,4 +11,4 @@ class ResCompany(models.Model):
     private_page_header_text = fields.Html(
         "Private Page Header Text", translate=True, sanitize=True
     )
-    qualification_information = fields.Text()
+    qualification_information = fields.Text(help="This information will be displayed as an instruction for the Qualification in Private Info.")
